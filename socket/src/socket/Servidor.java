66460a55a1debd9c18096116b9e3ee016304cc73
Leticia Campos <leticia.silva105@fatec.sp.gov.br>
2019-08-21 17:40:19 -0300

    criacao da comunicacao do cliente

--- socket/src/socket/Servidor.java
+++ socket/src/socket/Servidor.java
@@ -1,49 +1,46 @@
 package socket;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.Scanner;
-import java.io.OutputStream;
 
 public class Servidor {
-
 	public static void main(String[] args) {
-		try {
-			System.out.println("Servidor iniciado..");
-			ServerSocket server = new ServerSocket(17777);
-			System.out.println("Porta reservada, aguardando comunicação do cliente");
-			Socket s = server.accept(); //aqui ele para esperando alguem se comunicar
+		try (ServerSocket srv = new ServerSocket(17777)) {
+			System.out.println("Servidor Iniciado");
+			 
+			System.out.println(
+					"Porta reservada, " +
+					"aguardando comunicação do cliente");
+			Socket s = srv.accept();
 			OutputStream out = s.getOutputStream();
-			byte[] bytes = "Bem Vindo ao Servidor :D\r\n".getBytes();
+			InputStream in = s.getInputStream();
+			byte[] bytes = 
+				"Bem vindo ao servidor\r\n".getBytes();
 			out.write( bytes );
 			out.flush();
- 			Scanner scan = new Scanner(System.in);
- 			String texto = "";
- 			int i = 0;
- 			
- 			while(i != 27) {
- 				if(System.in.available() > 0) {
- 					i = System.in.read();
- 					out.write( i );
- 					out.flush();
- 				}
- 				
- 				if(System.in.available() < 0) {
- 					char c = (char)System.in.read();
- 					System.out.println(c);
- 				}
- 			}
- 			
-			System.out.println("Cliente conectado.");
+			System.out.println("Cliente conectado");
+			InputStreamReader reader = new InputStreamReader(System.in);
+			BufferedReader bfr = new BufferedReader(reader);
+			String linha = "";
+			while(!linha.contains("SAIR")) { 
+				if (bfr.ready()) { 
+					linha = bfr.readLine() + "\r\n";
+					out.write( linha.getBytes() );
+					out.flush();
+				}
+				if (in.available() > 0) {
+					char c = (char)in.read();
+					System.out.print(c);
+				}
+			}
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
-		System.out.println("Servidor finalizado!");
-		// pra fazer o teste basta ir no navegador e colocar o IP e a porta, ex: http://192.168.56.1:7777/
-
-		//instalar o telnet no recursos e programa, faz o teste telnet 192.168.56.1 17777
+		System.out.println("Servidor Finalizado");
 	}
-
 }
